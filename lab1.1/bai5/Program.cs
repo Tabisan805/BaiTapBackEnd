93ae0b03924207240ba703464fc12dd09451c75f
Hoàng Lã Việt <lahoangprotknl@gmail.com>
2025-04-01 15:51:28 +0700

    commit

---
+++ lab1.1/bai5/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x, y;
+Console.WriteLine("Nhap 2 so ");
+x = int.Parse(Console.ReadLine());
+y = int.Parse(Console.ReadLine());
+Console.WriteLine($"tong cua 2 so la {x + y} va tich cua 2 so la {x * y}");
