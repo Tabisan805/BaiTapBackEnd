fffbdf12b3930f44d4ce97f0a73c9e1ab5b6437d
Hoàng Lã Việt <lahoangprotknl@gmail.com>
2025-05-26 14:37:26 +0700

    lab 1.2 added
    Signed-off-by: Hoàng Lã Việt <lahoangprotknl@gmail.com>

--- lab1.1/bai5/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int x, y;
-Console.WriteLine("Nhap 2 so ");
-x = int.Parse(Console.ReadLine());
-y = int.Parse(Console.ReadLine());
-Console.WriteLine($"tong cua 2 so la {x + y} va tich cua 2 so la {x * y}");
